ef7cae7682e9923adacd34f57ce8b334116547f7
Oatimeany <ueaangkun.inyaem@gmail.com>
2023-10-16 16:20:57 +0700

    add: 7th day

--- 07 - Konami Code/start.js
+++ 07 - Konami Code/start.js
@@ -1,4 +1,33 @@
 (() => {
   // เริ่มเขียนโค้ด
+  const konamiCode = [
+    'ArrowUp',
+    'ArrowUp',
+    'ArrowDown',
+    'ArrowDown',
+    'ArrowLeft',
+    'ArrowRight',
+    'ArrowLeft',
+    'ArrowRight',
+    'b',
+    'a'
+  ]
 
+  let index = 0;
+
+  function onKeyDown(event) {
+    event.key ===konamiCode[index] ? index++ : index = 0;
+
+    if (konamiCode.length === index){
+      // we can call this function from other file as long as we defined the file that contain the function in index.html
+      startSnowing();
+    }
+  }
+
+  function run() {
+
+    // keydown will listen to what key on the keyboard we are pressing
+    document.addEventListener('keydown', onKeyDown);
+  }
+  run();
 })();
