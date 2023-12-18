171165d65444f609b5c95ede5b58bcf8b9cd2851
Oatimeany <ueaangkun.inyaem@gmail.com>
2023-12-18 15:56:46 +0700

    add: 10th day

--- 10 - Kanban Board/start.js
+++ 10 - Kanban Board/start.js
@@ -1,4 +1,48 @@
 (() => {
   // เริ่มเขียนโค้ด
+  let draggingElem;
 
+  function onDragStart() {
+    // Set draggingElem to this
+    console.log(this);
+
+    // *this* refers to the task element.
+    draggingElem = this;
+    console.log('drag');
+  }
+
+  function onDrop() {
+    // *this* refers to the element on which the event listener is attached, which is a drop zone element (.drop-zone).
+    this.append(draggingElem);
+    console.log(this);
+    draggingElem = null;
+  }
+
+  function onDragOver(event) {
+    // Prevent default behavior of DragOver
+    event.preventDefault();
+  }
+
+  function onDragEnter(event) {
+    // Prevent default behavior of DragEnter
+    event.preventDefault();
+  }
+
+  function run() {
+    // Covert from node to array
+    const taskElems = Array.from(document.querySelectorAll('.task'));
+    const dropZoneElems = Array.from(document.querySelectorAll('.drop-zone'));
+
+    taskElems.forEach((taskElem) => {
+      taskElem.addEventListener('dragstart', onDragStart);
+    });
+
+    dropZoneElems.forEach((dropZoneElem) => {
+      dropZoneElem.addEventListener('drop', onDrop);
+      dropZoneElem.addEventListener('dragover', onDragOver);
+      dropZoneElem.addEventListener('drangenter', onDragEnter);
+    });
+  }
+
+  run();
 })();
